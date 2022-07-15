2a73fb8abff692f1a9108af351d42fdef29ce390
Waleed1100x <107954669+Waleed1100x@users.noreply.github.com>
2022-07-15 16:02:57 +0300

    Phone Finder edit

---
+++ assets/js/phoneFinder.js
@@ -0,0 +1,41 @@
+let brandMenu = document.getElementById("brandMenu");
+let Connectivity = document.getElementById("Connectivity");
+
+//brand menu display
+let brandArray = [];
+phone.forEach((data) => {
+    if(!brandArray.includes(data.brand)){
+        brandArray.push(data.brand);
+    }
+});
+for(var i = 0;i<brandArray.length;i++){
+    brandMenu.innerHTML += 
+   ` <option value="${brandArray[i]}">${brandArray[i]}</option>`
+;
+}
+// end of brand menu
+
+// Connectivity menu display
+let connectivityArray = [];
+phone.forEach((data) => {
+    if(!connectivityArray.includes(data.Connectivity)){
+        connectivityArray.push(data.Connectivity);
+    }
+});
+for(var i = 0;i<connectivityArray.length;i++){
+    Connectivity.innerHTML += 
+   ` <option value="${connectivityArray[i]}">${connectivityArray[i]}</option>`
+;
+}
+//end of Connectivity menu
+
+function getSelectedValue(id){
+    let selectedValue = document.getElementById(`${id}`).value;
+    console.log(selectedValue);
+
+}
+function valueTest(){
+
+    console.log(document.getElementById("customRange2").value);
+    document.getElementById("hello").innerText = document.getElementById("customRange2").value
+}
